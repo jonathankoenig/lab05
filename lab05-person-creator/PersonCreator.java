040724d54df366b8819b2f26a6032267c0f064b8
JONATHAN KOENIG <jonathankoenig@adelphi.internal>
2016-09-15 11:46:41 -0400

    Added comments + fixed stuff

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -1,13 +1,11 @@
-
 /**
- * Write a description of class PersonCreator here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This class creates a person object with the details name, age, sex, height, weight, ssn, address, phone number, and date of birth"
  */
 public class PersonCreator
 {
-    // Your class begins here!
+    // ******************************
+    // Field Variables
+    // These set the variables for the details of the person
     private String name;
     private int age;
     private String sex;
@@ -18,134 +16,162 @@ public class PersonCreator
     private String phoneNum;
     private String dateOfBirth;
     
+    // *****************************
+    // Constructor
+    // Creates a person and acts the user to input a first and last name
+    // Also sets all the other details of the person to either 0 or Unknown
     public PersonCreator(String firstName, String lastName)
     {
         name = firstName + " " + lastName;
+        age = 0;
+        sex = "Unknown";
+        height = "Unknown";
+        weight = 0;
+        ssn = "Unknown";
+        address = "Unknown";
+        phoneNum = "Unknown";
+        dateOfBirth = "Unknown";
     }
     
+    // *****************************
+    // Methods
+    // These let the user input the various details for the perzon.
+    // There is also a method that lets the user see what these details are.
+    
+    // Sets the age of the person
     public void setAge(int getAge)
     {
         age = getAge;
         
-        if(age > 150)
+        // If the age inputed is less than 0 or greater than 150, an error is given and the age is changed to 0.
+        if(age > 150 || age < 0)
         {
-            System.out.println("Invalid Age!");
+            System.err.println("Invalid Age!");
             age = 0;
         }
     }
     
+    // Sets the sex of a person
     /**
-     * Put M for Male or F for Female
+     * Put "M" for Male or "F" for Female
      */
     public void setSex(String getSex)
     {
+        // If "M" is inputed, sex is set to Male
         if(getSex == "M")
         {
             sex = "Male";
         }
         
-        if(getSex == "F")
+        // If "F" is inputed, sex is set to Female
+        else if(getSex == "F")
         {
             sex = "Female";
         }
         
-        if(getSex != "M" || getSex != "F")
+        // If neither "M" or "F" is inputed, then an error is given and sex is changed to null
+        else
         {
-            System.out.println("Incorrect input! Input needs to be M or F.");
-            sex = "null";
+            System.err.println("Incorrect input! Input needs to be M or F.");
         }
     }
     
+    // Sets the height of the person in feet and inches
     public void setHeight(int getFt, int getIn)
     {
         height = getFt + "'" + getIn;
     }
     
+    // Sets the weight of the person
     public void setWeight(int getWeight)
     {
         weight = getWeight;
     }
     
+    // Sets the SSN of the person
     public void setSSN(int getssn1, int getssn2, int getssn3)
     {
-        
-        ssn = getssn1 + "-" + getssn2 + "-" + getssn3;
-        
+        // If the first part of the ssn is less than 0 or greater than 999, an error is given
         if(getssn1 < 0 || getssn1 > 999)
         {
-            System.out.println("Invalid input! First input must be between 0 and 999."); 
-            ssn = "null";
+            System.err.println("Invalid input! First input must be between 0 and 999."); 
         }
         
-        if(getssn2 < 0 || getssn2 > 99)
+        // If the second part of the ssn is less than 0 or greater than 99, an error is given
+        else if(getssn2 < 0 || getssn2 > 99)
         {
-            System.out.println("Invalid input! Second input must be between 0 and 99."); 
-            ssn = "null";
+            System.err.println("Invalid input! Second input must be between 0 and 99."); 
         }
         
-        if(getssn3 < 0 || getssn3 > 9999)
+        // If the third part of the ssn is less than 0 or greater than 9999, an error is given
+        else if(getssn3 < 0 || getssn3 > 9999)
         {
-            System.out.println("Invalid input! Third input must be between 0 and 9999."); 
-            ssn = "null";
+            System.err.println("Invalid input! Third input must be between 0 and 9999."); 
         }
         
+        // If every part of the SSN is valid, then the ssn is saved to the person
+        else
+        {
+            ssn = getssn1 + "-" + getssn2 + "-" + getssn3;
+        }
     }
     
+    
+    // Sets the address of the person by asking for house number, street name, city name, and state name
     public void setAddress(int getHouseNum, String getStreet, String getCity, String getState)
     {
         
         address = getHouseNum + " " + getStreet + ", " + getCity + " " + getState;
         
+        // If the house number is less than 0 or greater than 99999, then an error is given and the address is chagned to Unknown
         if(getHouseNum < 0 && getHouseNum > 99999)
         {
-            System.out.println("Invalid input! House number must be between 0 and 99999."); 
-            address = "null";
+            System.err.println("Invalid input! House number must be between 0 and 99999."); 
+            address = "Unknown";
         }
         
     }
     
+    // Sets the phone number of the person
     public void getPhoneNum(int getPhoneNum1, int getPhoneNum2, int getPhoneNum3)
     {
-        
-        phoneNum = getPhoneNum1 + "-" + getPhoneNum2 + "-" + getPhoneNum3;
-        
-        if(getPhoneNum1 < 0 && getPhoneNum1 > 999)
+        // If the first part of the number is less than 100 or greater than 999, then an error is given
+        if(getPhoneNum1 < 100 && getPhoneNum1 > 999)
         {
-            System.out.println("Invalid input! First number must be between 0 and 999.");
-            phoneNum = "null";
+            System.err.println("Invalid input! First number must be between 0 and 999.");
         }
-        
-        if(getPhoneNum2 < 0 && getPhoneNum2 > 999)
+        // If the second part of the number is less than 100 or greater than 999, then an error is given
+        else if(getPhoneNum2 < 100 && getPhoneNum2 > 999)
         {
-            System.out.println("Invalid input! Second number must be between 0 and 999.");
-            phoneNum = "null";
+            System.err.println("Invalid input! Second number must be between 0 and 999.");
         }
-        
-        if(getPhoneNum3 < 0 && getPhoneNum3 > 9999)
+        // If the third part of the number is less than 100 or greater than 999, then an error is given
+        else if(getPhoneNum3 < 1000 && getPhoneNum3 > 9999)
         {
-            System.out.println("Invalid input! Third number must be between 0 and 9999.");
-            phoneNum = "null";
+            System.err.println("Invalid input! Third number must be between 0 and 9999.");
+        }
+        // If every part of the number is valid, then the phone number is saved to the person
+        else
+        {
+            phoneNum = getPhoneNum1 + "-" + getPhoneNum2 + "-" + getPhoneNum3;
         }
     }
     
+    // Sets the date of birth of the person
     public void getDateOfBirth(int getDay, String getMonth, int getYear)
     {
        
         dateOfBirth = getMonth + " " + getDay + ", " + getYear;
         
-        if(getYear < 1850 || getYear > 2016)
-        {
-            System.out.println("Invalid year!");
-;
-        }
-        
-        if(getDay < 0 || getYear > 31)
+        // If the day is less than 0 or greater than 31, then an error is given
+        if(getDay < 0 || getDay > 31)
         {
             System.out.println("Invalid day!");
-            dateOfBirth = "null";
+            dateOfBirth = "Unknown";
         }
     }
     
+    // Prints out all the info of the person
     public void printInfo()
     {
         System.out.println("Name: " + name);
